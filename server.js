dafe5110f599c53e975354a75e0534063d197a28
Sam Freeman <41429405+nouaryk@users.noreply.github.com>
2020-01-16 16:55:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const WebSocket = require('ws');
 
-const wss = new WebSocket.Server({ port: 8081 });
+const wss = new WebSocket.Server({ port: process.env.port || 8081 });
 
 wss.on('connection', function connection(ws) {
     console.log('Connected')
